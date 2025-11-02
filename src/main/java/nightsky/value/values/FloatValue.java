510876031851029669dade10e49f9c00e0c6b97e
qm123pz <2184069042@qq.com>
2025-11-02 11:32:52 +0800

    Allow the value to be negative

--- src/main/java/nightsky/value/values/FloatValue.java
+++ src/main/java/nightsky/value/values/FloatValue.java
@@ -14,7 +14,7 @@ public class FloatValue extends Value<Float> {
     }
 
     public FloatValue(String string, Float value, Float minimum, Float maximum, BooleanSupplier check) {
-        super(string, value, floatV -> floatV >= 0 && floatV <= Float.MAX_VALUE, check);
+        super(string, value, floatV -> floatV >= minimum && floatV <= maximum, check);
         this.minimum = minimum;
         this.maximum = maximum;
     }
