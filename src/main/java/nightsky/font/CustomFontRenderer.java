c449ffce2af643f1298b4dfc94a3b231dee865cd
qm123pz <2184069042@qq.com>
2025-11-15 10:44:49 +0800

    fixed fontrenderer

--- src/main/java/nightsky/font/CustomFontRenderer.java
+++ src/main/java/nightsky/font/CustomFontRenderer.java
@@ -5,6 +5,7 @@ import org.lwjgl.opengl.GL11;
 
 import java.awt.*;
 import java.awt.font.FontRenderContext;
+import java.awt.font.GlyphVector;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
@@ -17,32 +18,26 @@ public class CustomFontRenderer {
     private static final FontRenderContext frc = new FontRenderContext(new AffineTransform(), true, true);
     private static final Map<Font, CustomFontRenderer> fontRenderers = new HashMap<>();
     
-    private final byte[][] charwidth = new byte[256][];
-    private final int[] textures = new int[256];
+    private final int[] charWidth = new int[65536];
+    private final int[] textures = new int[4096];
     private Font font;
-    private int fontWidth;
-    private int fontHeight; 
-    private int textureWidth;
-    private int textureHeight;
+    private int fontHeight;
+    private int textureWidth = 1024;
+    private int textureHeight = 1024;
     
     private CustomFontRenderer(Font font) {
         this.font = font;
         Arrays.fill(textures, -1);
         Rectangle2D maxBounds = font.getMaxCharBounds(frc);
-        this.fontWidth = (int) Math.ceil(maxBounds.getWidth());
         this.fontHeight = (int) Math.ceil(maxBounds.getHeight());
-        this.textureWidth = resizeToOpenGLSupportResolution(fontWidth * 16);
-        this.textureHeight = resizeToOpenGLSupportResolution(fontHeight * 16);
+
+        for (int i = 0; i < 65536; i++) {
+            charWidth[i] = (int) Math.ceil(font.getStringBounds(String.valueOf((char) i), frc).getWidth());
+        }
     }
     
     public static CustomFontRenderer getRenderer(Font font) {
         return fontRenderers.computeIfAbsent(font, CustomFontRenderer::new);
-    }
-    
-    private int resizeToOpenGLSupportResolution(int size) {
-        int power = 0;
-        while (size > 1 << power) power++;
-        return 1 << power;
     }
     
     public static void drawString(String text, float x, double y, int color, Font font) {
@@ -80,29 +75,85 @@ public class CustomFontRenderer {
     }
     
     private int drawChar(char chr, float x, double y) {
-        int region = chr >> 8;
-        int id = chr & 0xFF;
-        int xTexCoord = (id & 0xF) * fontWidth;
-        int yTexCoord = (id >> 4) * fontHeight;
-        int width = getOrGenerateCharWidthMap(region)[id];
+        int page = chr >> 4;
+        int id = chr & 0xF;
+        int xTexCoord = (id & 0xF) * 64;
+        int yTexCoord = (id >> 4) * 64;
+        int width = charWidth[chr];
         
-        GlStateManager.bindTexture(getOrGenerateCharTexture(region));
+        GlStateManager.bindTexture(getOrGenerateCharTexture(page));
         GlStateManager.enableTexture2D();
         GlStateManager.enableBlend();
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
         
         GL11.glBegin(GL11.GL_QUADS);
-        GL11.glTexCoord2d(wrapTextureCoord(xTexCoord, textureWidth), wrapTextureCoord(yTexCoord, textureHeight));
+        GL11.glTexCoord2d((double) xTexCoord / textureWidth, (double) yTexCoord / textureHeight);
         GL11.glVertex2f(x, (float) y);
-        GL11.glTexCoord2d(wrapTextureCoord(xTexCoord, textureWidth), wrapTextureCoord(yTexCoord + fontHeight, textureHeight));
-        GL11.glVertex2f(x, (float) (y + fontHeight));
-        GL11.glTexCoord2d(wrapTextureCoord(xTexCoord + width, textureWidth), wrapTextureCoord(yTexCoord + fontHeight, textureHeight));
-        GL11.glVertex2f(x + width, (float) (y + fontHeight));
-        GL11.glTexCoord2d(wrapTextureCoord(xTexCoord + width, textureWidth), wrapTextureCoord(yTexCoord, textureHeight));
+        GL11.glTexCoord2d((double) xTexCoord / textureWidth, (double) (yTexCoord + 64) / textureHeight);
+        GL11.glVertex2f(x, (float) (y + 64));
+        GL11.glTexCoord2d((double) (xTexCoord + width) / textureWidth, (double) (yTexCoord + 64) / textureHeight);
+        GL11.glVertex2f(x + width, (float) (y + 64));
+        GL11.glTexCoord2d((double) (xTexCoord + width) / textureWidth, (double) yTexCoord / textureHeight);
         GL11.glVertex2f(x + width, (float) y);
         GL11.glEnd();
         
         return width;
+    }
+    
+    private int getOrGenerateCharTexture(int page) {
+        if (textures[page] == -1)
+            return textures[page] = generateCharTexture(page);
+        return textures[page];
+    }
+    
+    private int generateCharTexture(int page) {
+        int textureId = GL11.glGenTextures();
+        int offset = page << 4;
+
+        BufferedImage img = new BufferedImage(textureWidth, textureHeight, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = (Graphics2D) img.getGraphics();
+
+        g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        g.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        
+        g.setFont(font);
+        g.setColor(Color.WHITE);
+
+        for (int y = 0; y < 16; y++) {
+            for (int x = 0; x < 16; x++) {
+                char ch = (char) ((y << 4 | x) | offset);
+                String chr = String.valueOf(ch);
+                int charX = x * 64;
+                int charY = y * 64;
+
+                GlyphVector gv = font.createGlyphVector(frc, chr);
+                g.drawGlyphVector(gv, charX, charY + g.getFontMetrics().getAscent());
+            }
+        }
+        
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureId);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_CLAMP);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_CLAMP);
+        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, textureWidth, textureHeight, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, imageToBuffer(img));
+        
+        g.dispose();
+        return textureId;
+    }
+    
+    private static ByteBuffer imageToBuffer(BufferedImage img) {
+        int[] arr = img.getRGB(0, 0, img.getWidth(), img.getHeight(), null, 0, img.getWidth());
+        ByteBuffer buf = ByteBuffer.allocateDirect(4 * arr.length);
+        for (int i : arr) {
+            buf.putInt(i << 8 | i >> 24 & 0xFF);
+        }
+        buf.flip();
+        return buf;
     }
     
     public static void drawStringWithShadow(String text, float x, float y, int color, Font font) {
@@ -118,64 +169,6 @@ public class CustomFontRenderer {
         drawString(text, x, y, color, font);
     }
     
-    private int getOrGenerateCharTexture(int id) {
-        if (textures[id] == -1)
-            return textures[id] = generateCharTexture(id);
-        return textures[id];
-    }
-    
-    private byte[] getOrGenerateCharWidthMap(int id) {
-        if (charwidth[id] == null)
-            return charwidth[id] = generateCharWidthMap(id);
-        return charwidth[id];
-    }
-    
-    private double wrapTextureCoord(int coord, int size) {
-        return coord / (double) size;
-    }
-    
-    private int generateCharTexture(int id) {
-        int textureId = GL11.glGenTextures();
-        int offset = id << 8;
-        BufferedImage img = new BufferedImage(textureWidth, textureHeight, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g = (Graphics2D) img.getGraphics();
-        g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-        g.setFont(font);
-        g.setColor(Color.WHITE);
-        FontMetrics fontMetrics = g.getFontMetrics();
-        for (int y = 0; y < 16; y++)
-            for (int x = 0; x < 16; x++) {
-                String chr = String.valueOf((char) ((y << 4 | x) | offset));
-                g.drawString(chr, x * fontWidth, y * fontHeight + fontMetrics.getAscent());
-            }
-        GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureId);
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
-        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, textureWidth, textureHeight, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, imageToBuffer(img));
-        return textureId;
-    }
-    
-    private byte[] generateCharWidthMap(int id) {
-        int offset = id << 8;
-        byte[] widthmap = new byte[256];
-        for (int i = 0; i < widthmap.length; i++) {
-            widthmap[i] = (byte) Math.ceil(font.getStringBounds(String.valueOf((char) (i | offset)), frc).getWidth());
-        }
-        return widthmap;
-    }
-    
-    private static ByteBuffer imageToBuffer(BufferedImage img) {
-        int[] arr = img.getRGB(0, 0, img.getWidth(), img.getHeight(), null, 0, img.getWidth());
-        ByteBuffer buf = ByteBuffer.allocateDirect(4 * arr.length);
-        for (int i : arr) {
-            buf.putInt(i << 8 | i >> 24 & 0xFF);
-        }
-        buf.flip();
-        return buf;
-    }
-    
     public static int getStringWidth(String text, Font font) {
         if (text == null || text.isEmpty() || font == null) {
             return 0;
@@ -185,7 +178,7 @@ public class CustomFontRenderer {
         int width = 0;
         char[] chars = text.toCharArray();
         for (char chr : chars) {
-            width += renderer.getOrGenerateCharWidthMap(chr >> 8)[chr & 0xFF];
+            width += renderer.charWidth[chr];
         }
         return width / 2;
     }
