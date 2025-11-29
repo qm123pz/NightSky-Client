124258a33e55185dbd4c883e654b1dbab64afc15
qm123pz <2184069042@qq.com>
2025-11-29 21:14:38 +0800

    Added Prediction InventoryMove Disabler

--- src/main/java/nightsky/module/modules/misc/Disabler.java
+++ src/main/java/nightsky/module/modules/misc/Disabler.java
@@ -29,22 +29,19 @@ public class Disabler extends Module {
     @Override
     public void onEnabled() {
         if (this.mode.getModeString().equals("PredictionInventory")) {
-            ChatUtil.sendFormatted(String.format("%s%s: You can use Vanilla-InvWalk or Silent-InvManager now", NightSky.clientName, this.getName()));
+            ChatUtil.sendFormatted(String.format("%s%s: You can use Vanilla-InvWalk and Silent-InvManager now", NightSky.clientName, this.getName()));
         }
         packetsQueue.clear();
-        //ChatUtil.sendFormatted(String.format("%s%s: Disabler enabled - Queue cleared", NightSky.clientName, this.getName()));
     }
 
     @Override
     public void onDisabled() {
         if (!packetsQueue.isEmpty()) {
-            //ChatUtil.sendFormatted(String.format("%s%s: Sending %d queued packets before disable", NightSky.clientName, this.getName(), packetsQueue.size()));
             for (Packet packet : packetsQueue) {
                 PacketUtil.sendPacketNoEvent(packet);
             }
             packetsQueue.clear();
         }
-        //ChatUtil.sendFormatted(String.format("%s%s: Disabler disabled", NightSky.clientName, this.getName()));
     }
 
     @EventTarget
@@ -55,21 +52,16 @@ public class Disabler extends Module {
             Packet packet = event.getPacket();
             
             if (packet instanceof C16PacketClientStatus) {
-                //ChatUtil.sendFormatted(String.format("%s%s: Intercepted C16PacketClientStatus - Queue size: %d", NightSky.clientName, this.getName(), packetsQueue.size() + 1));
                 event.setCancelled(true);
                 packetsQueue.add(packet);
             } else if (packet instanceof C0EPacketClickWindow) {
-                //ChatUtil.sendFormatted(String.format("%s%s: Intercepted C0EPacketClickWindow - Queue size: %d", NightSky.clientName, this.getName(), packetsQueue.size() + 1));
-                event.setCancelled(true);
                 packetsQueue.add(packet);
             } else if (packet instanceof C0DPacketCloseWindow) {
                 if (!packetsQueue.isEmpty()) {
-                    //ChatUtil.sendFormatted(String.format("%s%s: Closing window - Sending %d queued packets", NightSky.clientName, this.getName(), packetsQueue.size()));
                     for (Packet queuedPacket : packetsQueue) {
                         PacketUtil.sendPacketNoEvent(queuedPacket);
                     }
                     packetsQueue.clear();
-                    //ChatUtil.sendFormatted(String.format("%s%s: Queue cleared after window close", NightSky.clientName, this.getName()));
                 }
             }
         }
